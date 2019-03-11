b5fceb6d7a5e3d670e571c70b37996d03fccfaa9
Arian Allenson Valdez <Secretmapper16@gmail.com>
2019-03-11 20:49:11 +0800

    Make find_neighbors -> find_walkable_neighbors

--- pathfinding/grid/core.py
+++ pathfinding/grid/core.py
@@ -1,22 +1,25 @@
-def is_inside(grid, node):
-    return node[0] >= 0 and node[0] < grid.shape[1] and node[1] >= 0 and node[1] < grid.shape[0]
-
-def find_neighbors(grid, node):
+def find_walkable_neighbors(grid, node):
     neighbors = []
 
-    if is_inside(grid, (node[0] - 1, node[1])):
+    if is_valid_neighbor(grid, (node[0] - 1, node[1])):
         neighbors.append((node[0] - 1, node[1]))
 
-    if is_inside(grid, (node[0] + 1, node[1])):
+    if is_valid_neighbor(grid, (node[0] + 1, node[1])):
         neighbors.append((node[0] + 1, node[1]))
 
-    if is_inside(grid, (node[0], node[1] - 1)):
+    if is_valid_neighbor(grid, (node[0], node[1] - 1)):
         neighbors.append((node[0], node[1] - 1))
 
-    if is_inside(grid, (node[0], node[1] + 1)):
+    if is_valid_neighbor(grid, (node[0], node[1] + 1)):
         neighbors.append((node[0], node[1] + 1))
 
     return neighbors
+
+def is_valid_neighbor(grid, node):
+    return is_inside(grid, node) and is_coord_walkable(grid, node)
+
+def is_inside(grid, node):
+    return node[0] >= 0 and node[0] < grid.shape[1] and node[1] >= 0 and node[1] < grid.shape[0]
 
 def is_coord_walkable(grid, node):
     return grid[node[1], node[0]] == 1
