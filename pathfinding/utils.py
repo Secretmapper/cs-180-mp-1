d1aafc09befa7eae56acf9051299500a01fae245
Arian Allenson Valdez <Secretmapper16@gmail.com>
2019-03-12 18:04:12 +0800

    Support render demo in utils

--- pathfinding/utils.py
+++ pathfinding/utils.py
@@ -8,5 +8,13 @@ def render_image(img_array, display_size=(1000, 500)):
 def render_anim(ani, **kwargs):
     return display_animation(ani)
 
+def render_demo(out):
+    print(' Start: ', out[0])
+    print(' End: ', out[1])
+    print(' Number of Expanded Nodes: ', out[2])
+    print(' Cost: ', out[3])
+    print(' Running time: %s seconds' % out[4])
+    print('\n')
+
 def render_anim_to_video(ani):
     return HTML(ani.to_html5_video())
